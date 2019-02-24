e2af5de351c8c8c3a370b5207f51e6631bbaa914
hkchook <hkchook@yahoo.com>
2019-02-24 22:54:33 +1300

    Add files via upload

---
+++ GeekHunters.Entities/Candidate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GeekHunters.Entities
+{
+    public partial class Candidate
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int? SkillId { get; set; }
+
+        public virtual Skill Skill { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ GeekHunters.Entities/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace GeekHunters.Entities
+{
+    public partial class Skill
+    {
+        public Skill()
+        {
+            Candidate = new HashSet<Candidate>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Candidate> Candidate { get; set; }
+    }
+}
