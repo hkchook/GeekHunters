cf1497d50c0db50035b81f4a86560345f35a0d47
hkchook <hkchook@yahoo.com>
2019-02-24 22:57:54 +1300

    Add files via upload

---
+++ GeekHunters.Services.Interfaces/ICandidateService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using GeekHunters.Entities;
+using System.Threading.Tasks;
+
+namespace GeekHunters.Services.Interfaces
+{
+    public interface ICandidateService
+    {
+        Task<IEnumerable<Candidate>> GetCandidates();
+
+        Task<Candidate> GetCandidate(int Id);
+
+        Task<IEnumerable<Candidate>> GetCandidatesBySkillId(int Id);
+
+        Task<bool> UpdateCandidate(Candidate candidate);
+
+        Task<IEnumerable<Skill>> GetSkills();
+
+        Task<int> AddCandidate(Candidate candidate);
+
+        Task<bool> DeleteCandidate(int id);
+    }
+}
