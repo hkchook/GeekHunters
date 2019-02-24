0fd6321b71654473dcc058c930f38950d7d3fb0d
hkchook <hkchook@yahoo.com>
2019-02-24 22:56:23 +1300

    Add files via upload

---
+++ GeekHunters.Repositories.Interfaces/ICandidateRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GeekHunters.Entities;
+
+namespace GeekHunters.Repositories.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<IEnumerable<Candidate>> GetCandidates();
+
+        Task<Candidate> GetCandidate(int Id);
+
+        Task<IEnumerable<Candidate>> GetCandidatesBySkillId(int Id);
+
+        Task<bool> UpdateCandidate(Candidate candidate);
+
+        Task<IEnumerable<Skill>> GetSkills();
+
+        Task<int> AddCandidate(Candidate candidate);
+
+        Task<bool> DeleteCandidate(int id);
+    }
+}
